73b6fd6dab91a291b02cdbf44c06b057467d13bf
DINANJANA B.N <105732109+L4Y3R@users.noreply.github.com>
2024-03-09 11:21:13 +0530

    updates

--- index.js
+++ index.js
@@ -64,6 +64,17 @@ const resolvers = {
       db.games.push(game);
       return game;
     },
+
+    updateGame(_, args) {
+      db.games = db.games.map((g) => {
+        if (g.id === args.id) {
+          return { ...g, ...args.edits };
+        }
+        return g;
+      });
+
+      return (db.games = db.games.filter((g) => g.id === args.id));
+    },
   },
 };
 
--- schema.js
+++ schema.js
@@ -28,12 +28,18 @@ export const typeDefs = `#graphql
     }
 
     type Mutation {
-        addGame(game: AddGameInput): Game
+        addGame(game: AddGameInput!): Game
         deleteGame(id: ID!): [Game]
+        updateGame(id: ID!, edits: EditGameInput!): Game
     }
 
     input AddGameInput{
         title: String!
         platform: [String!]!
+    }
+
+    input EditGameInput{
+        title: String
+        platform: [String!]
     }
 `;
